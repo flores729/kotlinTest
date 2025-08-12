78401a87b97ffb2ed50d8ba1194b10d209590cd8
flores <dmma729.gmail.com>
2025-08-12 11:53:25 +0900

    test init

---
+++ src/Main.kt
@@ -0,0 +1,46 @@
+fun main(args: Array<String>) {
+    println("hello world!")
+    var a = 10;
+    val b = 11;
+    //b는  수정 불가 읽기만 가능
+    var i : Int = 10;
+    var j : Int? = 10;
+    //i = null; 불가능 null값이 들어가려면 ? 존재해야함
+    j = null;
+    val s = "abc";
+    val I = 1;
+    val l = 1L;
+    val d = 1.0
+    val f = 1.0f
+    println("s = $s")
+    println("I = $I")
+    println("l = $l")
+    println("d = $d")
+    println("f = $f")
+    val priceA: Int = 200
+    val priceB: Int = 100
+    if (priceA > priceB) {
+        println("$priceA is bigger than $priceB")
+        println("--------------------")
+        //println("" + priceA + " is bigger than " + priceB)
+    }
+    else{
+        println("$priceB is bigger than $priceA")
+    }
+
+    if(j == null){
+        println("null check true")
+
+    }
+    else{
+        println("null check false")
+    }
+
+    val price: Int = 1000
+    if(price in arrayOf(100, 200, 300, 1000)){
+        println("contain")
+    }
+    else{
+        print("not contain")
+    }
+}
